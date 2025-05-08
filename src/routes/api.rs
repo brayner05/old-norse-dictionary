75b8f543cc1702bfee760ec7a21086510982b070
Brayden Rayner <braydenrayner@outlook.com>
2025-05-08 17:09:43 -0300

    Added API routes, and specifically one to get all words, and then created frontend code to request the API

--- src/routes/api.rs
+++ src/routes/api.rs
@@ -1,6 +1,25 @@
-use rocket::{Route, routes};
+use diesel::{query_dsl::methods::SelectDsl, RunQueryDsl, SelectableHelper};
+use rocket::{get, routes, Route, State};
+use rocket::serde::json::Json;
+
+use crate::{database::DbPool, models::word::Word};
+
+
+#[get("/word")]
+fn get_all_words(pool: &State<DbPool>) -> Json<Vec<Word>> {
+    use crate::schema::word::dsl::word;
+
+    let mut db_connection = pool.get()
+        .expect("Could not find a database connection");
+
+    let words = word.select(Word::as_select())
+        .load::<Word>(&mut db_connection)
+        .unwrap_or(vec![]);
+
+    Json(words)
+}
 
 
 pub fn routes() -> Vec<Route> {
-    routes![]
+    routes![get_all_words]
 }
--- static/js/admin.js
+++ static/js/admin.js
@@ -7,19 +7,48 @@ const admin_config = {
 const admin_buttons = document.querySelectorAll("main.admin-panel ul button")
 
 const admin_panel = {
-    active_panel: admin_buttons[0]
+    active_button: admin_buttons[0],
+    content_panel: document.getElementById("content")
 }
 
 for (const button of admin_buttons) {
-    button.addEventListener("click", e => {
-        admin_panel.active_panel.classList.remove("active")
+    button.addEventListener("click", async e => {
+        if (e.target == admin_panel.active_button) {
+            return
+        }
+
+        admin_panel.active_button.classList.remove("active")
         button.classList.add("active")
-        admin_panel.active_panel = button
+        admin_panel.active_button = button
+
+        await switch_panel()
     })
 }
 
 const get_all_words = async () => {
-    const response = await fetch(`${admin_config.api_url}/words`)
+    const response = await fetch(`${admin_config.api_url}/word`)
     const data = await response.json()
     return data
+}
+
+const switch_panel = async () => {
+    let words = []
+
+    switch (admin_panel.active_button.id) {
+        case "admin-button-view-entries": {
+            words = await get_all_words()
+        }
+    }
+
+    const list = document.createElement("div")
+    list.classList.add("content-list")
+
+    for (const word of words) {
+        const item = document.createElement("div")
+        item.classList.add("content-list-item")
+        item.innerText = word
+        list.appendChild(item)
+    }
+
+    admin_panel.content_panel.appendChild(list)
 }
